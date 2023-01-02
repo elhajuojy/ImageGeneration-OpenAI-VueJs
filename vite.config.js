a03b84da79d7692dcf3bca0f3b0223c3d0de6282
ELMAHDI ELHJUOJY <86893073+elhajuojy@users.noreply.github.com>
2023-01-02 22:54:09 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base:"/ImageGeneration-OpenAI-VueJs/",
+  base:"/ImageGeneration-OpenAI-VueJs/",
   plugins: [vue()],
   resolve: {
     alias: {
